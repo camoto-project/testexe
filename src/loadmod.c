79a650c2903c928a14009db83d034d5e8940ef82
Adam Nielsen <malvineous@shikadi.net>
2021-09-05 16:14:23 +1000

    Add C code placeholder

---
+++ src/loadmod.c
@@ -0,0 +1,10 @@
+/*
+ * This file must have DOS CRLF line endings or it won't compile.
+ */
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("This is a test.\n");
+	return 0;
+}
